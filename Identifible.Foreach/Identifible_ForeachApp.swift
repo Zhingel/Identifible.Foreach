8c915fc07d1a7effb9d2ec6253047d0c07cba33d
Stas <staszhingel@icloud.com>
2021-01-13 06:56:22 +0300

    Initial Commit

---
+++ Identifible.Foreach/Identifible_ForeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  Identifible_ForeachApp.swift
+//  Identifible.Foreach
+//
+//  Created by Стас Жингель on 13.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Identifible_ForeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
